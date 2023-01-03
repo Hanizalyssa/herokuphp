3939cd35a5cdcc73ff4f920f5d7b1a99d4c2db9b
Hanizalyssa <20007201@myrp.edu.sg>
2023-01-03 22:20:11 +0800

    chang font size

--- web/index.php
+++ web/index.php
@@ -1,9 +1,14 @@
-<meta name ="viewport">
+<meta name ="viewport" content="width=device-width, initial-scale=1">
 <html lang="en">
     <title> A brief title </title>
 <head>
+    <style>
+        p.a {
+            font-size: 12px;
+        }
+        </style>
 </head>
 <body>
-  Hello World!
+  <p class="a">Hello World!</p>
 </body>
 </html>
